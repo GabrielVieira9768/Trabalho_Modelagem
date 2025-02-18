3f2a61f19f8c289a052b89a2910285bf9ea1dd95
GabrielVieira9768 <gabrieloliveira9768@gmail.com>
2025-02-17 22:26:03 -0300

    começa crud de projetos

--- app/Http/Controllers/OngController.php
+++ app/Http/Controllers/OngController.php
@@ -8,10 +8,10 @@ use Illuminate\Http\Request;
 
 class OngController extends Controller
 {
-    // Retorna todas as ONGs para serem exibidadas na view
+    // Retorna todas as ONGs em ordem descendente para serem exibidadas na view
     public function index()
     {
-        $ongs = Ong::paginate(10);
+        $ongs = Ong::orderBy('created_at', 'desc')->paginate(10);
         return view ('ongs.index', compact('ongs'))->with('paginate', true);
     }
 }
